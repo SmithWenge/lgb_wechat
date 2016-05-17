7349470de1ca5328c9e7f8709ed87aaa0d837dff
weinipingfan <weinipingfan@gmail.com>
2016-05-17 17:29:03 +0800

    TEST article message test

--- src/main/java/com/lgb/wechat/function/weixin/servlet/ServletWeixinSupport.java
+++ src/main/java/com/lgb/wechat/function/weixin/servlet/ServletWeixinSupport.java
@@ -2,13 +2,10 @@ package com.lgb.wechat.function.weixin.servlet;
 
 import com.github.sd4324530.fastweixin.api.MediaAPI;
 import com.github.sd4324530.fastweixin.api.MessageAPI;
-import com.github.sd4324530.fastweixin.api.entity.Article;
 import com.github.sd4324530.fastweixin.api.enums.MediaType;
 import com.github.sd4324530.fastweixin.api.response.GetSendMessageResponse;
 import com.github.sd4324530.fastweixin.api.response.UploadMediaResponse;
-import com.github.sd4324530.fastweixin.message.BaseMsg;
-import com.github.sd4324530.fastweixin.message.MpNewsMsg;
-import com.github.sd4324530.fastweixin.message.TextMsg;
+import com.github.sd4324530.fastweixin.message.*;
 import com.github.sd4324530.fastweixin.message.req.MenuEvent;
 import com.github.sd4324530.fastweixin.message.req.TextReqMsg;
 import com.github.sd4324530.fastweixin.servlet.WeixinSupport;
@@ -43,22 +40,24 @@ public class ServletWeixinSupport extends WeixinSupport {
         String eventKey = event.getEventKey();
 
         if (eventKey.equals(ConstantsCollection.MENU_CJCX_KEY)) {
-            sendCJCXMessage();
+            Article article1 = new Article("文章消息测试标题1", "这是一个测试文章的描述1", "http://www.56team.com/photo/index/welcome001.jpg", "http://www.56team.com");
+            Article article2 = new Article("文章消息测试标题2", "这是一个测试文章的描述2", "http://www.56team.com/photo/index/welcome001.jpg", "http://www.56team.com");
+            return new NewsMsg(Arrays.asList(article1, article2));
         }
 
         return new TextMsg("你好");
     }
 
     private void sendCJCXMessage() {
-        MediaAPI mediaAPI = new MediaAPI(ConstantsCollection.APPCONFIG);
-        UploadMediaResponse response = mediaAPI.uploadMedia(MediaType.IMAGE, new File("/Users/liunaidi/Documents/tmp.png"));
-        String media_id = response.getMediaId();
-        Article article = new Article(media_id, "测试用户", "群发测试", "http://www.baidu.com", "群发测试", "群发测试", Article.ShowConverPic.NO);
-        UploadMediaResponse uploadMediaResponse = mediaAPI.uploadNews(Arrays.asList(article));
-        MpNewsMsg mpNewsMsg = new MpNewsMsg();
-        mpNewsMsg.setMediaId(uploadMediaResponse.getMediaId());
-        MessageAPI messageAPI = new MessageAPI(ConstantsCollection.APPCONFIG);
-        GetSendMessageResponse messageResponse = messageAPI.sendMessageToUser(mpNewsMsg, true, "0", null);
-        LOG.info("Send Message Id is " + messageResponse.getMsgId());
+//        MediaAPI mediaAPI = new MediaAPI(ConstantsCollection.APPCONFIG);
+//        UploadMediaResponse response = mediaAPI.uploadMedia(MediaType.IMAGE, new File("/Users/liunaidi/Documents/tmp.png"));
+//        String media_id = response.getMediaId();
+//        Article article = new Article(media_id, "测试用户", "群发测试", "http://www.baidu.com", "群发测试", "群发测试", Article.ShowConverPic.NO);
+//        UploadMediaResponse uploadMediaResponse = mediaAPI.uploadNews(Arrays.asList(article));
+//        MpNewsMsg mpNewsMsg = new MpNewsMsg();
+//        mpNewsMsg.setMediaId(uploadMediaResponse.getMediaId());
+//        MessageAPI messageAPI = new MessageAPI(ConstantsCollection.APPCONFIG);
+//        GetSendMessageResponse messageResponse = messageAPI.sendMessageToUser(mpNewsMsg, true, "0", null);
+//        LOG.info("Send Message Id is " + messageResponse.getMsgId());
     }
 }
--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -1,5 +1,17 @@
 package test;
 
+import com.alibaba.fastjson.JSON;
+import com.github.sd4324530.fastweixin.api.MediaAPI;
+import com.github.sd4324530.fastweixin.api.MessageAPI;
+import com.github.sd4324530.fastweixin.api.entity.Article;
+import com.github.sd4324530.fastweixin.api.enums.MediaType;
+import com.github.sd4324530.fastweixin.api.response.BaseResponse;
+import com.github.sd4324530.fastweixin.api.response.GetSendMessageResponse;
+import com.github.sd4324530.fastweixin.api.response.UploadMediaResponse;
+import com.github.sd4324530.fastweixin.message.MpNewsMsg;
+//import com.github.sd4324530.fastweixin.util.JSONUtil;
+import com.github.sd4324530.fastweixin.util.JSONUtil;
+import com.lgb.wechat.arc.util.constants.ConstantsCollection;
 import com.lgb.wechat.arc.util.date.DateUtils;
 import com.lgb.wechat.function.support.mongodb.MongoTemplate;
 import com.mongodb.Block;
@@ -8,10 +20,12 @@ import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
 
+import java.io.File;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.util.Locale;
+import java.util.*;
 
+import static com.github.sd4324530.fastweixin.util.JSONUtil.DEFAULT_FORMAT;
 import static java.util.Arrays.asList;
 
 public class Test {
@@ -50,6 +64,21 @@ public class Test {
 //        });
 //        client.close();
 
-        System.out.println(DateUtils.nowYMD());
+//        System.out.println(DateUtils.nowYMD());
+
+        MediaAPI mediaAPI = new MediaAPI(ConstantsCollection.APPCONFIG);
+        UploadMediaResponse response = mediaAPI.uploadMedia(MediaType.IMAGE, new File("/Users/liunaidi/Documents/1.png"));
+        String media_id = response.getMediaId();
+        Article article = new Article(media_id, "测试用户", "群发测试", "http://www.baidu.com", "群发测试", "群发测试", Article.ShowConverPic.NO);
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("articles", Arrays.asList(article));
+        System.out.println(JSONUtil.toJson(map));
+
+        UploadMediaResponse uploadMediaResponse = mediaAPI.uploadNews(Arrays.asList(article));
+        MpNewsMsg mpNewsMsg = new MpNewsMsg();
+        mpNewsMsg.setMediaId(uploadMediaResponse.getMediaId());
+        MessageAPI messageAPI = new MessageAPI(ConstantsCollection.APPCONFIG);
+        GetSendMessageResponse messageResponse = messageAPI.sendMessageToUser(mpNewsMsg, true, "0", null);
+        System.out.println("Send Message Id is " + messageResponse.getMsgId());
     }
 }
